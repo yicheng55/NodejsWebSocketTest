f74831eb1462a3487198dd908a9902fcf141676f
Yi-Cheng <yich_liao@mail.davicom.com.tw>
2021-11-22 20:33:16 +0800

    init.

---
+++ index.js
@@ -0,0 +1,17 @@
+//使用 WebSocket 的網址向 Server 開啟連結
+let ws = new WebSocket('ws://localhost:3000')
+
+//開啟後執行的動作，指定一個 function 會在連結 WebSocket 後執行
+ws.onopen = () => {
+    console.log('open connection')
+}
+
+//關閉後執行的動作，指定一個 function 會在連結中斷後執行
+ws.onclose = () => {
+    console.log('close connection')
+}
+
+//接收 Server 發送的訊息
+ws.onmessage = event => {
+    console.log(event)
+}
